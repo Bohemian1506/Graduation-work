2546aba74f3e0274ce4e064c7235b0376b718832
Bohemian1506 <hiroshi151506@gmail.com>
2025-03-19 01:44:18 +0900

    ToDoモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_10_104409) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_16_153732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,17 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_10_104409) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "due_date"
+    t.integer "task_status"
+    t.bigint "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_tasks_on_event_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -57,4 +68,5 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_10_104409) do
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "tasks", "events"
 end
