bc94ab3b6bcc5180beecdf9aacfb37cf4f90a8c7
ZAKI <rq-Bohemian1506-at-321899771753>
2025-03-24 01:38:15 +0900

    ヘッダー実装（ハンバーガーボタン除く）

--- app/components/shared/header_component.rb
+++ app/components/shared/header_component.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Shared::HeaderComponent < ViewComponent::Base
-	def initialize(current_user: nil)
+    def initialize(current_user: nil)
     @current_user = current_user
-	end
+  end
 
   def logged_in?
     @current_user.present?
