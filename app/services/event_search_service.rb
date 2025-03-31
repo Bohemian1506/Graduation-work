d73f350311509e6fdfd9f75d086397144ce5d32b
Bohemian1506 <hiroshi151506@gmail.com>
2025-03-31 23:02:29 +0900

    ルボコップ修正

--- app/services/event_search_service.rb
+++ app/services/event_search_service.rb
@@ -1,7 +1,7 @@
 class EventSearchService
   attr_reader :params
 
-	# 空のハッシュを作成し、引数がない（not nil）場合でもエラーにならないようにする。
+  # 空のハッシュを作成し、引数がない（not nil）場合でもエラーにならないようにする。
   def initialize(params = {})
     @params = params
   end
@@ -24,12 +24,12 @@ class EventSearchService
     params[:q] && params[:q][:title_cont].present?
   end
 
-	# 検索ワードを受け取る
+  # 検索ワードを受け取る
   def keyword
     params[:q][:title_cont]
   end
 
-	# 受け取った検索ワードが各フィールドにないか検索し”or”で結合する
+  # 受け取った検索ワードが各フィールドにないか検索し”or”で結合する
   def perform_keyword_search
     Event.ransack(
       title_cont: keyword,
