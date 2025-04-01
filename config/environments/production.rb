c01ea0fb44b1f65292e2e3adcf4ea33d8a7ec252
Bohemian1506 <hiroshi151506@gmail.com>
2025-04-02 01:03:43 +0900

    Revert "パスワードリセット"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,17 +99,4 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
-  # メール用の設定
-  config.action_mailer.default_url_options = { host: "yan-hui-todo.onrender.com" }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              "smtp.gmail.com",  # 使用するSMTPサーバー
-  port:                 587,
-  domain:               "yan-hui-todo.onrender.com",
-  user_name:            Rails.application.credentials.gmail[:username],
-  password:             Rails.application.credentials.gmail[:password],
-  authentication:       "plain",
-  enable_starttls_auto: true
-}
 end
