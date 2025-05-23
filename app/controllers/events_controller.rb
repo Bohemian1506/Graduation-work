68569869d2609647aa17078615ce7cbb88559c49
Bohemian1506 <hiroshi151506@gmail.com>
2025-05-23 22:06:39 +0900

    セキュリティ警告の修正とページネーション機能の改善
    sanitize_page_paramメソッドによりページ番号を安全に処理し、
    Brakemanの偽陽性警告を適切に無視する設定を追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,9 @@ class EventsController < ApplicationController
     # app/services/event_search_service.rbに詳しく検索用のロジックをまとめてある
     search_service = EventSearchService.new(params)
     @q = search_service.search
-    @events = search_service.result.page(params[:page])
+    # sanitize_page_paramで必ずparams[:page]の値が1以上になるように設定。
+    page_number = sanitize_page_param(params[:page])
+    @events = search_service.result.page(page_number)
   end
 
   def show
@@ -68,5 +70,16 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:title, :event_date, :store_name, :location, :capacity, :party_fee, :party_type, :event_status, :visibility, :notes)
+  end
+
+  # ページ番号を安全にサニタイズ
+  def sanitize_page_param(page_param)
+    # page_paramが空白かチェック、空白なら1を代入してメソッド終了
+    return 1 if page_param.blank?
+    
+    # page_paramの文字列を整数にしてpage_numberに代入
+    page_number = page_param.to_i
+    # page_numberが0以下なら1を代入する
+    page_number > 0 ? page_number : 1
   end
 end
